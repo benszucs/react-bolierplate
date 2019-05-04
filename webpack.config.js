31f668ae1c79b0b59a6c1d0f5681372214af99b6
Bendeguz Szucs <benszucs@gmail.com>
2019-05-04 09:43:15 +0100

    update boileplate

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,10 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 module.exports = {
   mode: "production",
   entry: path.resolve(__dirname, "src") + "/index.js",
+  output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "bundle.js"
+	},
   module: {
     rules: [
       {
